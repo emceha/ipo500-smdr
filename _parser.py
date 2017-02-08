9af1924467ca25a9d0e4328bf0fe9b6be707185f
Mariusz Chrzanowski <em.chrzanowski@gmail.com>
2017-02-08 19:11:29 +0100

    Add files via upload

---
+++ _parser.py
@@ -0,0 +1,50 @@
+#! python
+#! coding: utf-8
+
+import csv
+from datetime import timedelta
+from collections import defaultdict
+
+# avaya ipo 500 smdr format but date and time separated!
+keys = ['DATE', 'TIME', 'DURATION', 'RING', 'CALLER', 'DIR', 'CALLED', 'DIALLED',
+        'ACC', 'EXT', 'CALLID', 'CONT', 'P1DEV', 'P1NAME', 'P2DEV', 'P2NAME']
+
+
+data = list(csv.reader(open('2017-01.log')))
+
+calls = (dict(zip(keys, row)) for row in data)
+
+# filter all outgoing externall calls 
+res = [c for c in calls if c['DIR'] == 'O' and c['EXT'] == '0' and c['P2NAME'].startswith('Line')]
+
+# for every caller collect pairs (phone number, call duration)
+d = defaultdict(list)
+for n in res:
+    caller = n['P1NAME']
+    called = n['CALLED']
+    (h, m, s) = n['DURATION'].split(':')
+    
+    delta = timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+    
+    if delta > timedelta():
+        d[caller].append((called, delta))
+
+# present results
+for n in d:
+    print n
+    print '+-----------+-----+----------+'
+    
+    dd = defaultdict(list)
+    for k, v in d[n]:
+        dd[k].append(v)
+
+    total = timedelta()
+    for k in dd:
+        sd = sum(dd[k], timedelta())
+        total += sd
+        print '| % 9s | % 3s | %  8s |' % (k, len(dd[k]), sd)
+
+    print '+-----------+-----+----------+'
+    print '            | % 3s | % 8s |' % (len(d[n]), total)
+    print '            +-----+----------+'
+  
---
+++ _smdr.py
@@ -0,0 +1,37 @@
+#! python
+
+from telnetlib import Telnet
+from datetime import datetime
+import logging
+
+count, lines = 0, 0
+
+logging.basicConfig(filename="smdr.log", format='%(levelname)s : %(asctime)s : %(message)s', level=logging.INFO)
+logging.info('begin ...')
+
+conn = Telnet('192.168.0.201', 8808) # ipo
+
+try:
+    while count < 4:
+        smdr = conn.read_until('\n', 5) # 5s timeout
+        
+        if smdr != "":
+            data = smdr.strip().split(',')
+            stamp = datetime.strptime(data[0], '%Y/%m/%d %H:%M:%S')
+            data = ','.join(data[0].split(' ') + data[1:])
+            print(data)
+    
+            filename = "%s-%s.log" % (stamp.strftime("%Y"), stamp.strftime("%m")) 
+            with open(filename, "a") as clog:
+                clog.write(data + '\n')
+        
+            lines += 1
+            count = 0
+        else:
+            count += 1
+    
+    logging.info('end, new records : %s' % lines)
+
+except Exception as err:
+    print(str(err))
+    logging.exception('aborted')
