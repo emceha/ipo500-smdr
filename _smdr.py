6b711030b386d6149d8133162a4a187492ffda65
Mariusz Chrzanowski <em.chrzanowski@gmail.com>
2017-07-31 17:51:48 +0200

    Update _smdr.py

--- _smdr.py
+++ _smdr.py
@@ -4,27 +4,31 @@
 from telnetlib import Telnet
 from datetime import datetime
 import logging
+import os
 
+header = "DATETIME,DURATION,RING,CALLER,DIR,CALLED,DIALED,ACC,EXT,CALLID,CONT,P1DEV,P1NAME,P2DEV,P2NAME,,,,,,,,,,,,,,,,IP1,PORT1,IP2,PORT2\n"
 count, lines = 0, 0
-
 logging.basicConfig(filename="smdr.log", format='%(levelname)s : %(asctime)s : %(message)s', level=logging.INFO)
 logging.info('begin ...')
 
-conn = Telnet('192.168.0.201', 8808) # ipo address and port, setup needed through ipo manager
+conn = Telnet('192.168.0.201', 8808) # ipo
 
 try:
-    while count < 4:
-        smdr = conn.read_until('\n', 5) # 5s timeout
+    while count < 5:
+        data = conn.read_until('\n', 3) # 3s timeout
         
-        if smdr != "":
-            data = smdr.strip().split(',')
-            stamp = datetime.strptime(data[0], '%Y/%m/%d %H:%M:%S')
-            data = ','.join(data[0].split(' ') + data[1:]) # separate date and time
+        if data != "":
+            stamp = datetime.strptime(data[:19], '%Y/%m/%d %H:%M:%S')
             print(data)
-            
-            filename = "%s-%s.log" % (stamp.strftime("%Y"), stamp.strftime("%m")) 
-            with open(filename, "a") as clog:
-                clog.write(data + '\n')
+    
+            filename = "%s-%s.log" % (stamp.strftime("%Y"), stamp.strftime("%m"))
+            if not os.path.exists(filename):
+                with open(filename, "w") as clog:
+                    clog.write(header)
+                    clog.write(data)
+            else:
+                with open(filename, "a") as clog:
+                    clog.write(data)
         
             lines += 1
             count = 0
