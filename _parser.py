18771a6d096b0b9790dbdb38b4332b5881712762
Mariusz Chrzanowski <em.chrzanowski@gmail.com>
2017-07-31 17:55:11 +0200

    Update _parser.py

--- _parser.py
+++ _parser.py
@@ -1,50 +1,53 @@
 #! python
 #! coding: utf-8
 
+import sys
 import csv
 from datetime import timedelta
 from collections import defaultdict
 
-# avaya ipo 500 smdr format but date and time separated!
-keys = ['DATE', 'TIME', 'DURATION', 'RING', 'CALLER', 'DIR', 'CALLED', 'DIALED',
-        'ACC', 'EXT', 'CALLID', 'CONT', 'P1DEV', 'P1NAME', 'P2DEV', 'P2NAME']
+# must be the first line of csv file
+# DATETIME,DURATION,RING,CALLER,DIR,CALLED,DIALED,ACC,EXT,CALLID,CONT,P1DEV,P1NAME,P2DEV,P2NAME,,,,,,,,,,,,,,,,IP1,PORT1,IP2,PORT2
 
+def main(logfile):
+    data = list(csv.reader(open('logfile')))
+    calls = (dict(zip(data[0], row)) for row in data[1:])
 
-data = list(csv.reader(open('2017-01.log')))
-
-calls = (dict(zip(keys, row)) for row in data)
-
-# filter all outgoing externall calls 
-res = [c for c in calls if c['DIR'] == 'O' and c['EXT'] == '0' and c['P2NAME'].startswith('Line')]
-
-# for every caller collect pairs (phone number, call duration)
-d = defaultdict(list)
-for n in res:
-    caller = n['P1NAME']
-    called = n['CALLED']
-    (h, m, s) = n['DURATION'].split(':')
+    # filter all outgoing external calls 
+    res = [c for c in calls if c['DIR'] == 'O' and c['P2NAME'].startswith('Line') and (len(c['CALLED']) > 8)]
     
-    delta = timedelta(hours=int(h), minutes=int(m), seconds=int(s))
-    
-    if delta > timedelta():
-        d[caller].append((called, delta))
+    # for every caller collect pairs (phone number, call duration)
+    d = defaultdict(list)
+    for n in res:
+        caller = n['P1NAME']
+        called = n['CALLED']
+        
+        (h, m, s) = n['DURATION'].split(':')
+        delta = timedelta(hours=int(h), minutes=int(m), seconds=int(s))
 
-# present results
-for n in d:
-    print n
-    print '+-----------+-----+----------+'
-    
-    dd = defaultdict(list)
-    for k, v in d[n]:
-        dd[k].append(v)
+        if delta > timedelta():
+            d[caller].append((called, delta))
 
-    total = timedelta()
-    for k in dd:
-        sd = sum(dd[k], timedelta())
-        total += sd
-        print '| % 9s | % 3s | %  8s |' % (k, len(dd[k]), sd)
-
-    print '+-----------+-----+----------+'
-    print '            | % 3s | % 8s |' % (len(d[n]), total)
-    print '            +-----+----------+'
+    # present results
+    for n in d:
+        print n
+        print '+-----------+-----+----------+'
   
+        dd = defaultdict(list)
+        for k, v in d[n]:
+            dd[k].append(v)
+
+        total = timedelta()
+        for k in dd:
+            sd = sum(dd[k], timedelta())
+            total += sd
+            print '| % 9s | % 3s | %  8s |' % (k, len(dd[k]), sd)
+
+        print '+-----------+-----+----------+'
+        print '            | % 3s | % 8s |' % (len(d[n]), total)
+        print '            +-----+----------+'
+
+  
+if __name__ == "__main__":
+    if len(sys.argv) > 1:    
+        main(sys.argv[1])
