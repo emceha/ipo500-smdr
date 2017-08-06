4174aa638a00d3e182c9d8328adf7ff06925b982
Mariusz Chrzanowski <em.chrzanowski@gmail.com>
2017-08-06 15:16:13 +0200

    Update _smdr.py

--- _smdr.py
+++ _smdr.py
@@ -1,27 +1,33 @@
-#! python
-# not compatible with python 3!
+#! python3
+#! coding: utf-8
 
 from telnetlib import Telnet
 from datetime import datetime
 import logging
 import os
 
-header = "DATETIME,DURATION,RING,CALLER,DIR,CALLED,DIALED,ACC,EXT,CALLID,CONT,P1DEV,P1NAME,P2DEV,P2NAME,,,,,,,,,,,,,,,,IP1,PORT1,IP2,PORT2\n"
+header = "DATETIME,DURATION,RING,CALLER,DIR,CALLED,DIALED,ACC,EXT,CALLID,CONT,\
+          P1DEV,P1NAME,P2DEV,P2NAME,,,,,,,,,,,,,,,,IP1,PORT1,IP2,PORT2\n"
 count, lines = 0, 0
-logging.basicConfig(filename="smdr.log", format='%(levelname)s : %(asctime)s : %(message)s', level=logging.INFO)
+
+logging.basicConfig(filename="smdr.log",
+                    format='%(levelname)s : %(asctime)s : %(message)s',
+                    level=logging.INFO)
+
 logging.info('begin ...')
 
 try:
     conn = Telnet('192.168.0.201', 8808) # ipo
-    
-    while count < 5:
-        data = conn.read_until('\n', 3).strip() # 3s timeout
-        
+
+    while count < 5: # 15s total timeout
+        data = conn.read_until(b'\n', 3).decode("utf-8").strip() # 3s timeout
+
         if data != "":
-            stamp = datetime.strptime(data[:19], '%Y/%m/%d %H:%M:%S')
             print(data)
-    
+
+            stamp = datetime.strptime(data[:19], '%Y/%m/%d %H:%M:%S')
             filename = "%s-%s.log" % (stamp.strftime("%Y"), stamp.strftime("%m"))
+            
             if not os.path.exists(filename):
                 with open(filename, "w") as clog:
                     clog.write(header)
@@ -29,12 +35,12 @@ try:
             else:
                 with open(filename, "a") as clog:
                     clog.write(data + '\n')
-        
+
             lines += 1
             count = 0
         else:
             count += 1
-    
+
     logging.info('end, new records : %s' % lines)
 
 except Exception as err:
