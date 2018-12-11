6a1ec8bd09ed166e8a7bbd26e6fb778e284bb906
emceha <em.chrzanowski@gmail.com>
2018-12-11 18:38:42 +0100

    sqlite

---
+++ csv2sql.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+import csv
+import sqlite3
+import hashlib
+from datetime import datetime
+
+
+COLUMNS = ("callstart,duration,ring,caller,dir,called,dialled,acc,"
+           "isinternal,callid,cont,p1device,p1name,p2device,p2name,"
+           "hold,park,authvalid,authcode,ucharged,charge,currency,"
+           "aocamount,callunits,aocunits,costperunit,markup,"
+           "extargcause,extargid,extargeted,ip1,port1,ip2,port2,"
+           "calluid")
+
+
+def row_exists(conn, crsr, cuid):
+    query = f"SELECT calluid FROM calls WHERE calluid=?"
+    crsr.execute(query, (cuid,))
+    fetch = crsr.fetchall()
+    conn.commit()
+    return fetch
+
+
+def insert_row(conn, crsr, columns, values, row):
+    query = f"INSERT INTO calls({columns}) VALUES({values})"
+    crsr.execute(query, row)
+    conn.commit()
+
+
+def create_table(conn, crsr, columns):
+    query = f"CREATE TABLE IF NOT EXISTS calls ({columns})"
+    crsr.execute(query)
+    conn.commit()
+
+
+def make_cuid(row):
+    cstr = f"{row[0]} {row[1]} {row[8]} {row[9]}"
+    cuid = hashlib.md5(cstr.encode("ascii")).hexdigest()
+    return cuid
+
+
+def csv_to_sql(db, csvfile):
+    with sqlite3.connect(db) as conn:
+        crsr = conn.cursor()
+
+        with open(csvfile, 'r', encoding="utf-8") as clog:
+            reader = csv.reader(clog)
+            next(reader)
+            strcols = COLUMNS
+            cols = strcols.split(',')
+            create_table(conn, crsr, strcols)
+
+            for row in reader:
+                cuid = make_cuid(row)
+                if not row_exists(conn, crsr, cuid):
+                    dt = row[0].replace('/', '-')
+                    row[0] = int(datetime.fromisoformat(dt).timestamp())
+                    row.append(cuid)
+                    values = ','.join('?' * len(cols))
+                    insert_row(conn, crsr, strcols, values, row)
+
+
+def main(dbpath, flist):
+    try:
+        for cf in flist:
+            sys.stdout.write(f'{cf} ... ')
+            sys.stdout.flush()
+            csv_to_sql(dbpath, cf)
+            sys.stdout.write('OK\n')
+    except Exception as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    if sys.argv[1:]:
+        if not os.path.exists('./db'):
+            os.makedirs('./db')
+        main('./db/smdr.db', sys.argv[1:])
--- smdr.py
+++ smdr.py
@@ -2,45 +2,78 @@
 
 import os
 import logging
+import sqlite3
+import hashlib
 from telnetlib import Telnet
+from datetime import datetime
 
-HEADER = ("callstart,duration,ring,caller,dir,called,dialled,acc,isinternal,"
-          "callid,cont,p1device,p1name,p2device,p2name,hold,park,authvalid,"
-          "authcode,ucharged,charge,currency,aocamount,callunits,aocunits,"
-          "costperunit,markup,extargcause,extargid,extargeted,"
-          "ip1,port1,ip2,port2")
 
-if not os.path.isdir("./log"):
-    os.makedirs("./log")
+COLUMNS = ("callstart,duration,ring,caller,dir,called,dialled,acc,"
+           "isinternal,callid,cont,p1device,p1name,p2device,p2name,"
+           "hold,park,authvalid,authcode,ucharged,charge,currency,"
+           "aocamount,callunits,aocunits,costperunit,markup,"
+           "extargcause,extargid,extargeted,ip1,port1,ip2,port2,"
+           "calluid")
 
-logging.basicConfig(filename="./log/smdr.log",
-                    format="%(levelname)s : %(asctime)s : %(message)s",
-                    level=logging.INFO)
 
-logging.info("begin ...")
+logging.basicConfig(
+    format="%(asctime)s [ %(levelname)-6s] %(message)s ",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO
+)
 
-try:
-    conn = Telnet('192.168.4.201', 8808)
 
-    while True:
-        data = conn.read_until(b'\n', 10).strip()
+def create_table(conn, crsr):
+    query = f"CREATE TABLE IF NOT EXISTS calls ({COLUMNS})"
+    crsr.execute(query)
+    conn.commit()
 
-        if not data:
-            break
-        else:
-            row = data.decode('utf-8')
-            year, month = row[:7].split('/')
-            filename = f"./log/{year}-{month}.csv"
 
-            if not os.path.exists(filename):
-                with open(filename, "w", encoding='utf-8') as clog:
-                    clog.write(HEADER + '\n')
-                    clog.write(row + '\n')
-            else:
-                with open(filename, "a", encoding='utf-8') as clog:
-                    clog.write(row + '\n')
+def get_row(adrs, port):
+    with Telnet(adrs, port, 15) as conn:
+        row = conn.read_until(b'\n', 10).strip()
+    return row.decode('utf-8')
 
-    logging.info(" ... done")
 
-except Exception:
-    logging.exception("aborted")
+def insert_row(conn, crsr, row):
+    values = ','.join('?' * len(row))
+    query = f"INSERT INTO calls({COLUMNS}) VALUES({values})"
+    crsr.execute(query, row)
+    conn.commit()
+
+
+def make_cuid(row):
+    cstr = f"{row[0]} {row[1]} {row[8]} {row[9]}"
+    cuid = hashlib.md5(cstr.encode("ascii")).hexdigest()
+    return cuid
+
+
+def main(db, address, port):
+    try:
+        with sqlite3.connect(db) as conn:
+            crsr = conn.cursor()
+            logging.info(f"database : '{db}'")
+            create_table(conn, crsr)
+            logging.info(f"ipo : {address}:{port}")
+
+            while True:
+                row = get_row(address, port)
+                if not row:
+                    break
+
+                row = row.split(',')
+                cuid = make_cuid(row)
+                row.append(cuid)
+
+                dt = row[0].replace('/', '-')
+                row[0] = int(datetime.fromisoformat(dt).timestamp())
+                insert_row(conn, crsr, row)
+                logging.info(f"{dt} : {cuid}")
+
+        logging.info("... done ")
+    except Exception as error:
+        logging.error(error)
+
+
+if __name__ == "__main__":
+    main('./db/smdr.db', '192.168.4.201', 8808)
