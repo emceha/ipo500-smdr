7aa5505a15972253e8252690dbc57a1188ad0093
emceha <em.chrzanowski@gmail.com>
2018-12-12 13:33:25 +0100

    backup log

--- smdr.py
+++ smdr.py
@@ -8,12 +8,20 @@ from telnetlib import Telnet
 from datetime import datetime
 
 
-COLUMNS = ("callstart,duration,ring,caller,dir,called,dialled,acc,"
-           "isinternal,callid,cont,p1device,p1name,p2device,p2name,"
-           "hold,park,authvalid,authcode,ucharged,charge,currency,"
-           "aocamount,callunits,aocunits,costperunit,markup,"
-           "extargcause,extargid,extargeted,ip1,port1,ip2,port2,"
-           "calluid")
+CNAMES = ("callstart,duration,ring,caller,dir,called,dialled,acc,"
+          "isinternal,callid,cont,p1device,p1name,p2device,p2name,"
+          "hold,park,authvalid,authcode,ucharged,charge,currency,"
+          "aocamount,callunits,aocunits,costperunit,markup,"
+          "extargcause,extargid,extargeted,ip1,port1,ip2,port2,"
+          "cheksum")
+
+DATATYPES = ("TEXT,TEXT,INTEGER,TEXT,TEXT,TEXT,TEXT,TEXT,INTEGER,"
+             "INTEGER,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,"
+             "TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,TEXT,"
+             "TEXT,INTEGER,TEXT,INTEGER,TEXT NOT NULL UNIQUE")
+
+COLUMNS = ','.join([f"{c} {d}" for c, d in zip(
+    CNAMES.split(','), DATATYPES.split(','))])
 
 
 logging.basicConfig(
@@ -24,55 +32,50 @@ logging.basicConfig(
 
 
 def create_table(conn, crsr):
-    query = f"CREATE TABLE IF NOT EXISTS calls ({COLUMNS})"
-    crsr.execute(query)
+    crsr.execute(f"CREATE TABLE IF NOT EXISTS calls({COLUMNS});")
     conn.commit()
 
 
 def get_row(adrs, port):
     with Telnet(adrs, port, 15) as conn:
         row = conn.read_until(b'\n', 10).strip()
-    return row.decode('utf-8')
+    return row
 
 
 def insert_row(conn, crsr, row):
     values = ','.join('?' * len(row))
-    query = f"INSERT INTO calls({COLUMNS}) VALUES({values})"
+    query = f"INSERT INTO calls({CNAMES}) VALUES({values})"
     crsr.execute(query, row)
     conn.commit()
 
 
-def make_cuid(row):
-    cstr = f"{row[0]} {row[1]} {row[8]} {row[9]}"
-    cuid = hashlib.md5(cstr.encode("ascii")).hexdigest()
-    return cuid
-
-
-def main(db, address, port):
+def main(db, log, address, port):
     try:
         with sqlite3.connect(db) as conn:
             crsr = conn.cursor()
-            logging.info(f"database : '{db}'")
             create_table(conn, crsr)
+
+            logging.info(f"database : '{db}'")
             logging.info(f"ipo : {address}:{port}")
 
-            while True:
-                row = get_row(address, port)
-                if not row:
-                    break
+            with open(log, 'ab') as bkp:
+                while True:
+                    row = get_row(address, port)
+                    if not row:
+                        break
 
-                row = row.split(',')
-                cuid = make_cuid(row)
-                row.append(cuid)
+                    bkp.write(row + b'\n')
+                    chksum = hashlib.sha1(row).hexdigest()
 
-                row[0] = row[0].replace('/', '-')
-                insert_row(conn, crsr, row)
-                logging.info(f"{cuid}")
+                    row = row.decode().replace('/', '-').split(',')
+                    row.append(chksum)
 
-        logging.info("... done ")
+                    insert_row(conn, crsr, row)
+
+        logging.info("... done")
     except Exception as error:
         logging.error(error)
 
 
 if __name__ == "__main__":
-    main('./db/smdr.db', '192.168.4.201', 8808)
+    main('./db/smdr.db', './log/calls.log', '192.168.4.201', 8808)
